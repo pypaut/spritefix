3b738a31d0e8c743be2119b0269ee1abad07c3cc
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-11-18 22:48:44 +0100

    Transparent background

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ import numpy as np
 
 from boxes import get_bounding_boxes, get_sprites, sort_boxes
 from mask import get_mask
+from PIL import Image
 
 
 def main():
-    print("Loading sheet and configuration...")
     sheet = cv2.imread("media/sheet.png")
     s = open("media/rows.txt", "r").read()[:-1]  # Remove newline
     rows = [int(n) for n in s.split(" ")]
@@ -15,20 +15,16 @@ def main():
     nb_cols = max(rows)
 
     # Get sprite positions from mask
-    print("Computing sprite positions...")
     mask = get_mask()
     boxes = get_bounding_boxes(mask)  # box is [x, y, w, h]
 
     # Sanity check
-    print("Checking coherence of results...")
     assert len(boxes) == sum(rows)
 
     # Sort boxes
-    print("Sorting and ordering sprite positions...")
     boxes = sort_boxes(boxes, rows)
 
     # Get sprites from initial sheet, using sorted boxes positions
-    print("Extracting sprites...")
     sprites = get_sprites(boxes, sheet)
 
     # Save each sprite
@@ -37,19 +33,16 @@ def main():
     #         cv2.imwrite(f"media/sprite_{i}{j}.png", sprites[i][j])
 
     # Get sprite max size
-    print("Computing new sprite spacing...")
     sizes = []
     for r in sprites:
         for s in r:
             sizes.append(len(s))
             sizes.append(len(s[0]))
-    sprite_size = int(max(sizes) * 1.5)
+    sprite_size = max(sizes) + 10
 
     # Create new sprite sheet
-    print("Creating new sprite sheet...")
     new_sheet = np.zeros((nb_rows * sprite_size, nb_cols * sprite_size, 3))
 
-    print("Copying sprites to new sheet...")
     # From bottom
     for i in range(nb_rows):
         for j in range(rows[i]):
@@ -60,10 +53,22 @@ def main():
                 pos_j: pos_j + sprites[i][j].shape[1],
             ] = sprites[i][j].copy()
 
-    print("Saving new sprite sheet...")
     cv2.imwrite("media/fixed_sheet.png", new_sheet)
 
-    print("Making transparent background...")
+    # Transparent background
+    img = Image.open('media/fixed_sheet.png')
+    img = img.convert("RGBA")
+    datas = img.getdata()
+
+    newData = []
+    for item in datas:
+        if item[0] == 0 and item[1] == 0 and item[2] == 0:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    img.save("media/transparent_sheet.png", "PNG")
 
     print("Done!")
 
